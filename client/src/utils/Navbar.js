887343e7cafe2fbeed7c28aef1ff3c9b5d4b7824
HamdiNawfel <hamdii.nawfel@gmail.com>
2021-01-07 20:45:52 +0100

    fix the height of logo

--- client/src/utils/Navbar.js
+++ client/src/utils/Navbar.js
@@ -394,8 +394,8 @@
                       {
                         !props.user.authenticated?
                         <Link to='/'color="inherit" style={{textDecoration:'none', display:'flex'}}>
-                          <img src={logo} alt ="logo" style={{ width: 50 ,marginTop:10}} className={classes.logo}/>
-                          <p style={{color:'#017a9b', fontSize:'2rem', margin:'0', marginTop:10, fontWeight:900, userSelect:'none'}}>drouci</p>
+                          <img src={logo} alt ="logo" style={{ width: 50, maxHeight:50 ,marginTop:10}} className={classes.logo}/>
+                          <p style={{color:'#017a9b', fontSize:'2rem', margin:'15px 0 0 0', marginTop:10, fontWeight:900, userSelect:'none'}}>drouci</p>
                         </Link>
                         :
                         <Link to='/dashboard' color="inherit" style={{textDecoration:'none', display:'flex'}}>
